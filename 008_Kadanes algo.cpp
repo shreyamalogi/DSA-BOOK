fc0ae3f6576e1710a8c364da29bc1de178585fa9
Shreya Christiana <5hre9a@gmail.com>
2021-04-29 01:09:28 +0530

    arrays

--- 008_Kadanes algo.cpp
+++ 008_Kadanes algo.cpp
@@ -1,7 +1,3 @@
-//author :shreyamalogi
-
-//kadanes algorithm
-//find the largest sum of contiguous subarray
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -16,17 +12,19 @@ int main()
 	 	cin>>n;
 	 	
 	 	int a[n],i;
+	 	for(i=0;i<n;i++)
 	 	cin>>a[i];
 	 	
 	 	long long int cs=a[0], ms=a[0]; //we need just 2 variables current sum and maximum sum
 	 	
 	 	for(i=1; i<n; i++)
 	 	{
-	 		cs=max(cs+a[i],a[i]); //current sum will be maximum of ce or ce +cs
-	 		cs=max(ms,cs);         //maximum element will be max of max sum or current sum
+	 		cs=max(cs,ms); //current sum will be maximum of ce or ce +cs
+	 		ms=max(ms,cs);         //maximum element will be max of max sum or current sum
 		 }
 		 cout<<ms<<endl;  //print max sum
 	 }
      return 0;
 }
+
 
