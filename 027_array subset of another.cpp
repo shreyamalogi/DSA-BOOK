5fbd30a513f14b488c78158ff674319b6f56bb6e
Shreya Christiana <5hre9a@gmail.com>
2021-05-01 14:22:58 +0530

    arrays

--- 027_array subset of another.cpp
+++ 027_array subset of another.cpp
@@ -1,7 +1,8 @@
+
 //author :shreyamalogi
 
-//array subset of another array
-//in interviews we actually cant use map but yea im using though
+//we actually have 4 methods of approach
+//but im using hashing unordered map approach cuz its optimized way but this maping i think we acnt use in interviews
 
 #include<bits/stdc++.h>
 
@@ -11,27 +12,36 @@ int main()
 {
 	int tt;
 	cin>>tt;
-	while(tt--)
-	{
-		long long int m,n;
-		cin>>m,n;
-		
-		long long int a[m],b[n],i;
-		unordered_map<long long int, long long int> m1,m2
-		for(i=0;i<m;i++)
-		{
-			cin>>a[i];
-			m1[a[i]]++;
-		}
-		
-		for(i=0;i<m;i++)
-		{
-			cin>>b[i];
-			m2[b[i]]++;
+	while(tt--){
+	int m,n;
+	cin>>m>>n;
+	int a[m],b[n];
+	for(int i=0;i<m;i++) cin>>a[i];
+	for(int i=0;i<n;i++) cin>>b[i];
+	
+	unordered_map<int,int>m1;
+	
+	for(int i=0;i<m;i++) m1[a[i]]++;
+	
+	int c=0;
+	for(int i=0;i<n;i++){
+		if(m1[b[i]]){
+			c++;
 		}
 	}
+	if(c!=n)cout<<"no"<<endl;
+	else cout<<"yes"<<endl;
+    }
      return 0;
 }
 
-//incomplete code....wait i will try without using unordered map
+//2
+//6 4
+//11 1 13 2 3 7
+//11 3 7 1
+//yes
+//53
+//10 5 2 23 19
+//19 5 6
+//no
 
