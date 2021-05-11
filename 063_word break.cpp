41940be698a7c205108d09cda6f87e49b76ba229
Shreya Christiana <5hre9a@gmail.com>
2021-05-11 11:25:27 +0530

    strings dp ltr

---
+++ 063_word break.cpp
@@ -0,0 +1,30 @@
+//author :shreyamalogi
+
+//Given a string A and a dictionary of n words B, find out if A can be segmented into a space-separated sequence of dictionary words. 
+
+// we are given a string......we need to break the strings anmd check whether the broken strings are found in the library
+
+//trying with recurssion but tcx is too high
+
+//we can do dp : cuz optimization of recursion is dp
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		string s;
+		cin>>s;
+		
+		
+		
+		
+	}
+     return 0;
+}
+
