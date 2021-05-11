86a5c4ce567e1ff8de0aaf9e1945e61032f10119
Shreya Christiana <5hre9a@gmail.com>
2021-05-11 12:25:33 +0530

    strings

---
+++ 067_count reversals.cpp
@@ -0,0 +1,19 @@
+//author :shreyamalogi
+
+//Given a string S consisting of only opening and closing curly brackets '{' and '}', find out the minimum number of reversals required to convert the string into a balanced expression.
+//A reversal means changing '{' to '}' or vice-versa.
+
+
+//already done this prob in parenthesis
+//same thing
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+     return 0;
+}
+
