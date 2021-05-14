bc317b25fc7f780b6bc5b4ed973ebc48e0fc48ce
Shreya Christiana <5hre9a@gmail.com>
2021-05-14 19:20:10 +0530

    strings idk

---
+++ 070_search a word in 2d grid.cpp
@@ -0,0 +1,31 @@
+//author :shreyamalogi
+
+//Find the string in grid
+//search a word in a 2d grid of charecters
+
+/*
+
+Given a 2D grid of n*m of characters and a word, 
+find all occurrences of given word in grid.
+ A word can be matched in all 8 directions 
+ at any point. Word is said be found in a 
+ direction if all characters match in this 
+ direction (not in zig-zag form).
+  The 8 directions are, horizontally left, 
+  horizontally right, vertically up,
+  vertically down and 4 diagonal directions.
+
+
+*/
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+     return 0;
+}
+
