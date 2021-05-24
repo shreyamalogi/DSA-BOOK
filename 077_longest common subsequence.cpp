6d0c0e9b65ad2f37cc4ade6dca8a571ff0b64dae
Shreya Christiana <5hre9a@gmail.com>
2021-05-24 12:27:39 +0530

    dp ltr

---
+++ 077_longest common subsequence.cpp
@@ -0,0 +1,19 @@
+//author :shreyamalogi
+
+/*
+
+check the last charecters for bothe string if they are equal the recursively call
+
+*/
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+     return 0;
+}
+
