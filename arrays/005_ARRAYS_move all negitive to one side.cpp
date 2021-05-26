9cf8a59ee27f3e34d4efb3f943856a609e9cbce5
Shreya Christiana <5hre9a@gmail.com>
2021-05-26 23:14:10 +0530

    arrays

--- arrays/005_ARRAYS_move all negitive to one side.cpp
+++ arrays/005_ARRAYS_move all negitive to one side.cpp
@@ -1,6 +1,20 @@
 //author :shreyamalogi
-//move all negitive elements to one side
 
+/*
+move all negitive to one side nad and all positive to other side
+
+if we suse brute force: 
+approach 1: sorting then tc will be more o(n logn) and space will be o(1) cuz we dont need extra space
+approach 2: take 2 arrays one array for +ve and other for -ve but tc will be o(n+n) and sc: o(n) cuz we use extra space for N and p
+approach 3 optimized: quick sort approach by using partition algo
+						pivot element means 0
+						i pointer will throw all eleemenst
+						keep updating i ki value if there is positive element
+approach 4 optimized: two pointer approach						
+*/
+
+
+//partition
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -14,7 +28,7 @@ int main()
   for(i=0; i<n; i++)
      cin>>a[i];
      
-    int j=0;              //take extra pointer j and initialize it with 0
+    int j=0;              //take extra pointer j and initialize it with 0 (pivot)
 	for(i=0; i<n; i++)   //scan/traverse through the array
 	{
 		if(a[i]<0)      //if we find a negitive element
