797064ee0d24789e7e6bd6f31184ead8897ecb91
Shreya Christiana Malogi <5hre9a@gmail.com>
2022-01-15 18:21:01 +0530

    arrays

--- arrays/004_ARRAYS_SORT without using algo.cpp
+++ arrays/004_ARRAYS_SORT without using algo.cpp
@@ -10,45 +10,51 @@ using namespace std;
 
 int main()
 {
-  int n;
-  cin>>n;
+  int n; cin>>n;
   
   int a[n],i;
-  for(i=0; i<n; i++)
-    cin>>a[i];
+  for(i=0; i<n; i++);  cin>>a[i];
+   
     
-   // we need 3 variables for counting 0 1 and 2
-   int c0=0, c1=0, c2=0; 
-   
-   for(i=0; i<n; i++)
-   {
-   	
-   	if (a[i]==0)
-   	c0++;
-   	
-   	else if(a[i]==1)
-   	c1++;
-   	
-   	else
-   	c2++;
-   	
-   }
-   
-   //first we will fill c0 then c1 thn c2 so 3 for loops
-   for(i=0; i<c0; i++)
-   a[i]=0;
-   
-   for(i=0; i<c0+c1; i++)
-   a[1]=1;
-   
-   while(c2--)
-   {
-   	 a[i++]=2;
-   }
-   
-   //finally print the result
-   for(i=0; i<n; i++)
-   cout<<a[i]<<" ";
-  return 0;
-  }
+    //init 3 count variables to 0
+    int c0=0,c1=1,c2=2;
+    
+    //use a loop till n and check conditions take array conditions and increment the respective counters
+    for(i=0;i<n;i++){
+    	if(a[i]==0){
+    		c0++;
+		}
+		else if(a[i]==1){
+			c1++;
+		}
+		else{
+			c2++;
+		}
+	}
+	
+	//use a loop till n and check i conditions with count and push the elements to their respective indices
+	for(i=0;i<n;i++){
+		if(i<c0){
+			a[i]=0;
+		}
+		else if(i<c0+c1){
+			a[i]=1;
+		}
+		else{
+			a[i]=2;
+		}
+	}
+    
+
+  for(i=0; i<n; i++) cout<<a[i]<<" ";
+    
+    return 0;
+}
+
+
+
+//5
+//1 0 2 1 2
+
+//0 1 1 1 2
 
