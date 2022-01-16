e8a5d3d6341db6950816ad6032d4411f87955be6
Shreya Christiana Malogi <5hre9a@gmail.com>
2022-01-16 23:10:02 +0530

    arrays

--- arrays/006_union of 2 arrays.cpp
+++ arrays/006_union of 2 arrays.cpp
@@ -8,6 +8,20 @@ we can use merge function: tc: o(m+n)
 
 */
 
+class Solution{
+    public:
+    //Function to return the count of number of elements in union of two arrays.
+    int doUnion(int a[], int n, int b[], int m)  {
+        set<int>s;
+        for(int i=0;i<n;i++)
+       s.insert(a[i]);
+       for(int i=0;i<m;i++)
+       s.insert(b[i]);
+       return s.size();
+   
+    }
+};
+
 
 //find the union and  of 2 sorted arrays
 
